4dbbb10d19295fb9f4beec11251567bab0aa2d59
a <skekf123@naver.com>
2019-12-11 22:24:28 +0900

    end webView

--- app/src/main/java/com/drj/a/myapplication/WebActivity.kt
+++ app/src/main/java/com/drj/a/myapplication/WebActivity.kt
@@ -1,14 +1,15 @@
 package com.drj.a.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Window
+import android.view.*
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.bm_input.*
 import kotlinx.android.synthetic.main.web_browser.*
-import org.jetbrains.anko.activityManager
-import org.jetbrains.anko.webView
+import org.jetbrains.anko.*
 import java.util.*
 
 /**
@@ -36,6 +37,75 @@ class WebActivity : AppCompatActivity() {
                 false
             }
         }
+
+        registerForContextMenu(webView)
+    }
+
+    //뒤로가기 동장 허용
+    override fun onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)  //menu를 나타내게 함
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId){
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("http://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("http://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("http://www.hanmail.net")
+                return true
+            }
+            R.id.action_call -> {
+//                val intent = Intent(Intent.ACTION_DIAL)
+//                intent.data = Uri.parse("tel: 031-705-7847")
+//                if(intent.resolveActivity(packageManager) != null){
+//                    startActivity(intent)
+//                }
+                makeCall("010-9012-7040")
+                return true
+            }
+            R.id.action_send_text -> {
+                sendSMS("010-9012-7040", webView.url)
+                return true
+            }
+            R.id.action_browser -> {
+                email("skekf123@naver.com", "만중이가 공유하는 사이트", webView.url)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_share -> {
+                share(webView.url)
+            }
+            R.id.action_browser -> {
+                browse(webView.url)
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 
 

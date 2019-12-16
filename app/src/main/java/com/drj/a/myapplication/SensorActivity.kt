5860e4f6fe59948238b2bb523e8bb8087c362783
a <skekf123@naver.com>
2019-12-16 21:45:42 +0900

    Sensor is ready

--- app/src/main/java/com/drj/a/myapplication/SensorActivity.kt
+++ app/src/main/java/com/drj/a/myapplication/SensorActivity.kt
@@ -1,48 +1,5 @@
 package com.drj.a.myapplication
 
-import android.content.Context
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-
 /**
- * Created by a on 2019. 12. 12..
+ * Created by a on 2019. 12. 16..
  */
-class SensorActivity : AppCompatActivity(), SensorEventListener {
-
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    private val sensorManager by lazy {
-        getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.sensor)
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                SensorManager.SENSOR_DELAY_NORMAL)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        sensorManager.unregisterListener(this)
-    }
-
-}
-

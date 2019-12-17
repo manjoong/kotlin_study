624a37e0c42d6ca20309f68dd9de3b9a4c6855bd
a <skekf123@naver.com>
2019-12-17 23:02:30 +0900

    complate sensor app

--- app/src/main/java/com/drj/a/myapplication/SensorActivity.kt
+++ app/src/main/java/com/drj/a/myapplication/SensorActivity.kt
@@ -1,5 +1,63 @@
 package com.drj.a.myapplication
 
+import android.app.Activity
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.WindowManager
+
 /**
- * Created by a on 2019. 12. 16..
+ * Created by a on 2019. 12. 12..
  */
+class SensorActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var tiltView: TiltView
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            Log.d("SensorActivity", "onSensorChanged" +
+                    " ${event.values[0]} = x, ${event.values[1]} = y, ${event.values[2]} = y")
+            tiltView.onSensorEvent(event)
+        }
+    }
+
+    private val sensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.sensor_activity)
+
+        tiltView = TiltView(this)
+        setContentView(tiltView)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+}

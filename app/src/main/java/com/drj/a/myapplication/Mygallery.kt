6b484efad98dba627c9153b3a5f32d2c26e7d564
a <skekf123@naver.com>
2019-12-18 22:51:46 +0900

    ready for gallery app

---
+++ app/src/main/java/com/drj/a/myapplication/Mygallery.kt
@@ -0,0 +1,16 @@
+package com.drj.a.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by a on 2019. 12. 18..
+ */
+class Mygallery : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_gallery)
+    }
+}

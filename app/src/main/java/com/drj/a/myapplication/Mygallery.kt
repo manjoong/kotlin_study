e9fe024488d6ffdf2896f34ee9fcad18cb908ba1
a <skekf123@naver.com>
2019-12-28 23:21:20 +0900

    add gallery code

--- app/src/main/java/com/drj/a/myapplication/Mygallery.kt
+++ app/src/main/java/com/drj/a/myapplication/Mygallery.kt
@@ -1,16 +1,78 @@
 package com.drj.a.myapplication
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
+import java.util.jar.Manifest
 
 /**
  * Created by a on 2019. 12. 18..
  */
 class Mygallery : AppCompatActivity() {
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.my_gallery)
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                alert("외부 권한이 필요합니다"){
+                    yesButton {
+                        ActivityCompat.requestPermissions(this@Mygallery,
+                                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                                REQUEST_READ_EXTERNAL_STORAGE)
+                    }
+                    noButton {  }
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            getAllPhotos()
+        }
     }
-}
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_READ_EXTERNAL_STORAGE -> {
+                if ((grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                    getAllPhotos()
+                }else{
+                    toast("권한 거부됨")
+                }
+                return
+            }
+        }
+    }
+
+    private fun getAllPhotos(){
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC")
+
+        if (cursor != null){
+            while (cursor.moveToNext()) {
+                var uri = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+                Log.d("Mygallery", uri)
+            }
+            cursor.close()
+            }
+        }
+    }

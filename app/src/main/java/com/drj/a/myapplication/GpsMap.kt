153c8f454cda7d493e2356db3d7d846b92745576
a <skekf123@naver.com>
2020-01-07 21:20:05 +0900

    start google gps app

---
+++ app/src/main/java/com/drj/a/myapplication/GpsMap.kt
@@ -0,0 +1,16 @@
+package com.drj.a.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by a on 2020. 1. 7..
+ */
+class GpsMap : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.gps_activity)
+
+    }
+}
